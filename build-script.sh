a8784710a1efc58d489cf7e7168b8b1f41961149
zylina <zylina.lion@gmail.com>
2020-10-06 10:56:38 -0400

    updated files

--- build-script.sh
+++ build-script.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
-yum update -y
-amazon-linux-extras install docker -y
-systemctl start docker
-systemctl enable docker
-usermod -a -G docker ec2-user
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+#updateing docker in the instance
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker 
+sudo usermod -a -G docker ec2-user
+#downloading docker-compose in the instance
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 TOKEN="xxxxxxxxx"
 FOLDER="https://$TOKEN@raw.githubusercontent.com/clarusway/clarusway-aws-devops-1-20/master/devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/"
 curl -s --create-dirs -o "/home/ec2-user/bookstore-api/bookstore-api.py" -L "$FOLDER"bookstore-api.py
